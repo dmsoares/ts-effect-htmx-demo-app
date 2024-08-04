338f001149c433076d3d9d7061701d5067384865
Decio Soares <decio.msoares@gmail.com>
2024-08-04 18:25:36 +0200

    Fix htmx header not being applied

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,25 +5,38 @@ import { Express } from "./express";
 import * as Browser from "./browser/handlers";
 import * as Api from "./api/handlers";
 
-const handler = (h: Handler) =>
+const setupHandler = (handler: Handler) =>
   Effect.gen(function* () {
     const runFork = Runtime.runFork(yield* Effect.runtime<never>());
-    return (req: Request, res: Response) => runFork(h(req, res));
+    return (req: Request, res: Response) =>
+      runFork(
+        Effect.mapError(handler(req, res), (e) =>
+          res.status(500).send(JSON.stringify({ error: e.message }))
+        )
+      );
   });
+
+const browserHandler = (handler: Handler) =>
+  setupHandler((req, res) =>
+    Effect.gen(function* () {
+      res.locals.useLayout = req.headers["hx-request"] !== "true";
+      return yield* handler(req, res);
+    })
+  );
 
 const BrowserRoutesLive = Layer.effectDiscard(
   Effect.gen(function* () {
     const app = yield* Express;
 
-    app.get("/", yield* handler(Browser.home));
-    app.get("/exams", yield* handler(Browser.exams));
-    app.get("/exams/create", yield* handler(Browser.examCreate));
-    app.get("/exams/:id", yield* handler(Browser.examDetail));
-    app.get("/exams/:id/edit", yield* handler(Browser.examEdit));
+    app.get("/", yield* browserHandler(Browser.home));
+    app.get("/exams", yield* browserHandler(Browser.exams));
+    app.get("/exams/create", yield* browserHandler(Browser.examCreate));
+    app.get("/exams/:id", yield* browserHandler(Browser.examDetail));
+    app.get("/exams/:id/edit", yield* browserHandler(Browser.examEdit));
     // partials
-    app.get("/partials/exams", yield* handler(Browser.listExams));
-    app.post("/partials/exams", yield* handler(Browser.createExam));
-    app.put("/partials/exams/:id", yield* handler(Browser.updateExam));
+    app.get("/partials/exams", yield* browserHandler(Browser.listExams));
+    app.post("/partials/exams", yield* browserHandler(Browser.createExam));
+    app.put("/partials/exams/:id", yield* browserHandler(Browser.updateExam));
   })
 );
 
@@ -31,9 +44,9 @@ const ApiRoutesLive = Layer.effectDiscard(
   Effect.gen(function* () {
     const app = yield* Express;
 
-    app.get("/api/exams", yield* handler(Api.listExams));
-    app.get("/api/exams/:id", yield* handler(Api.getExam));
-    app.post("/api/exams", yield* handler(Api.createExam));
+    app.get("/api/exams", yield* setupHandler(Api.listExams));
+    app.get("/api/exams/:id", yield* setupHandler(Api.getExam));
+    app.post("/api/exams", yield* setupHandler(Api.createExam));
   })
 );
 
